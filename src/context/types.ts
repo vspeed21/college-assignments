4fa3f44e4ce36c72026c4b189ad1bc226f2c517d
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-21 21:44:01 -0600

    add: TaskProvider types

--- src/context/types.ts
+++ src/context/types.ts
@@ -1,3 +1,12 @@
+import { ChangeEvent } from 'react';
+import { InfoUser } from '../interface';
+
 export type ContextTaskProps = {
-  saludo: string,
+  infoUser: InfoUser
+  setInfoUser: (state: InfoUser) => void
+  setValidQuestions: (state: boolean) => void,
+  validQuestions: boolean,
+  inputs: Array<[]>
+  handleSubjectValue: (e: ChangeEvent<HTMLInputElement>, i: number) => void,
+  subjectValues: string[],
 };

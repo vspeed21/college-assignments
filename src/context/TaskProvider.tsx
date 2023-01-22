efbbac32f09bdc9b23f0ed4775cbe21afd635182
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-21 21:44:41 -0600

    create: function to manage a dinamic input values

--- src/context/TaskProvider.tsx
+++ src/context/TaskProvider.tsx
@@ -1,4 +1,5 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+import { InfoUser } from '../interface';
 
 const TaskContext = createContext({});
 
@@ -7,11 +8,33 @@ interface Props {
 }
 
 export const TaskProvider = ({ children }:Props ) => {
+  const [infoUser, setInfoUser] = useState<InfoUser>({
+    name: '',
+    subject: '',
+    subjects: []
+  });
+  const [validQuestions, setValidQuestions] = useState(false);
+
+  // Generar el estado de cada uno de las materias dinamicamente
+  const inputs = Array.from({length: Number(infoUser.subject)}, (_, i) => i);
+  const [subjectValues, setSubjectValues] = useState<string[]>(inputs.map(() => ''));
+
+  function handleSubjectValue(e: React.ChangeEvent<HTMLInputElement>, i: number) {
+    const newInputValues = {...subjectValues};
+    newInputValues[i] = e.target.value;
+    setSubjectValues(newInputValues);
+  }
 
   return (
     <TaskContext.Provider
       value={{
-        saludo: 'holaaaaa'
+        infoUser,
+        setInfoUser,
+        setValidQuestions,
+        validQuestions,
+        inputs,
+        subjectValues,
+        handleSubjectValue
       }}
     >
       {children}

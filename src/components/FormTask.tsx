534940e33cb0bd2df07dc34815938fa4e040bf92
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-01-23 14:12:40 -0600

    add: infouser to up of form

--- src/components/FormTask.tsx
+++ src/components/FormTask.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState, useEffect } from 'react';
 import useTask from '../hooks/useTask';
 import { toast } from 'react-toastify';
 import TaskList from './TaskList';
+import ShowInfoUser from './ShowInfoUser';
 
 function FormTask() {
   const [nameTask, setNameTask] = useState('');
@@ -40,66 +41,70 @@ function FormTask() {
   };
 
   return (
-    <div
-      className='bg-white p-5 shadow-lg rounded-md mx-5 w-[600px] md:mx-auto'
-    >
-      <form
-        onSubmit={handleSubmit}
-      >
-        <div className="flex flex-col gap-3 mb-7">
-          <label htmlFor="name" className='uppercase font-bold text-gray-700'>Titulo Tarea</label>
-          <input
-            id='name'
-            type='text'
-            className="bg-gray-100 rounded focus:outline-blue-500 focus:shadow placeholder:text-gray-800 p-2 pl-4"
-            placeholder='Ingresa el titulo de la tarea'
-            value={nameTask}
-            onChange={e => setNameTask(e.target.value)}
-          />
-        </div>
+    <div className='flex flex-col'>
+      <ShowInfoUser/>
 
-        <div className='flex gap-4'>
-          <div className="flex flex-col gap-3 mb-3 w-1/2">
-            <label htmlFor="date" className='uppercase font-bold text-gray-700'>Fecha de entrega</label>
+      <div
+        className='bg-white p-5 shadow-lg rounded-md mx-5 w-[600px] md:mx-auto'
+      >
+        <form
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col gap-3 mb-7">
+            <label htmlFor="name" className='uppercase font-bold text-gray-700'>Titulo Tarea</label>
             <input
-              id='date'
-              type='date'
+              id='name'
+              type='text'
               className="bg-gray-100 rounded focus:outline-blue-500 focus:shadow placeholder:text-gray-800 p-2 pl-4"
-              value={dateTask}
-              onChange={e => setDateTask(e.target.value)}
-              min={minDate}
-              max='2024-01-23'
+              placeholder='Ingresa el titulo de la tarea'
+              value={nameTask}
+              onChange={e => setNameTask(e.target.value)}
             />
           </div>
-          <div className="flex flex-col gap-3 mb-3 w-1/2">
-            <label htmlFor="subject" className='uppercase font-bold text-gray-700'>Asignatura</label>
-            <select
-              id='subject'
-              className="bg-gray-100 rounded focus:outline-blue-500 focus:shadow placeholder:text-gray-800 p-2 pl-4 hover:cursor-pointer text-center capitalize"
-              value={subjectTask}
-              onChange={e => setSubjectTask(e.target.value)}
-            >
-              <option value=''>{'-- Seleccione --'}</option>
-              {valuesInputs.map(input => (
-                <option
-                  key={input}
-                  value={input}
-                  className='capitalize'
-                >
-                  {input}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
 
-        <input
-          type='submit'
-          value={task?.nameTask ? 'Guardar Cambios': 'Agregar Tarea'}
-          className='bg-blue-600 px-4 py-2 rounded-md hover:bg-blue-700 transition-colors text-white font-bold mt-3 hover:cursor-pointer w-full uppercase'
-        />
-      </form>
-      <TaskList/>
+          <div className='flex gap-4'>
+            <div className="flex flex-col gap-3 mb-3 w-1/2">
+              <label htmlFor="date" className='uppercase font-bold text-gray-700'>Fecha de entrega</label>
+              <input
+                id='date'
+                type='date'
+                className="bg-gray-100 rounded focus:outline-blue-500 focus:shadow placeholder:text-gray-800 p-2 pl-4"
+                value={dateTask}
+                onChange={e => setDateTask(e.target.value)}
+                min={minDate}
+                max='2024-01-23'
+              />
+            </div>
+            <div className="flex flex-col gap-3 mb-3 w-1/2">
+              <label htmlFor="subject" className='uppercase font-bold text-gray-700'>Asignatura</label>
+              <select
+                id='subject'
+                className="bg-gray-100 rounded focus:outline-blue-500 focus:shadow placeholder:text-gray-800 p-2 pl-4 hover:cursor-pointer text-center capitalize"
+                value={subjectTask}
+                onChange={e => setSubjectTask(e.target.value)}
+              >
+                <option value=''>{'-- Seleccione --'}</option>
+                {valuesInputs.map(input => (
+                  <option
+                    key={input}
+                    value={input}
+                    className='capitalize'
+                  >
+                    {input}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <input
+            type='submit'
+            value={task?.nameTask ? 'Guardar Cambios': 'Agregar Tarea'}
+            className='bg-blue-600 px-4 py-2 rounded-md hover:bg-blue-700 transition-colors text-white font-bold mt-3 hover:cursor-pointer w-full uppercase'
+          />
+        </form>
+        <TaskList/>
+      </div>
     </div>
   );
 }
